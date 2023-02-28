a08021adf0b02b03e092dca0190cf9921b5c4466
Leandro Sousa <lgleandro14@gmail.com>
2023-02-28 20:37:00 -0300

    Exercicio 1 finalizado

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,16 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//
+
+let indexOfPortfolio = menu.indexOf('Home');
+
+console.log(indexOfPortfolio);
+
+//
+
+menu.push('Contato');
+
+console.log(menu);
