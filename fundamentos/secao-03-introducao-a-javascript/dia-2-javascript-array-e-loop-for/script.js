e93d29bcaef17db88cbca435322f1cfca2bc86b5
Leandro Sousa <lgleandro14@gmail.com>
2023-02-28 21:28:37 -0300

    Finalizando exercicios do course

--- fundamentos/secao-03-introducao-a-javascript/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-2-javascript-array-e-loop-for/script.js
@@ -14,3 +14,32 @@ console.log(indexOfPortfolio);
 menu.push('Contato');
 
 console.log(menu);
+
+//
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+  console.log(groceryList[index])
+}
+
+//
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let name of names) {
+  console.log(name);
+}
+
+//
+
+let fruits = [3, 4, 10, 1, 12];
+let soma = 0;
+
+for (let index = 0; index < fruits.length; index += 1) {
+  soma += fruits[index];
+} if (soma > 15) {
+  console.log(soma);
+} else {
+  console.log('Valor menor que 16')
+}
