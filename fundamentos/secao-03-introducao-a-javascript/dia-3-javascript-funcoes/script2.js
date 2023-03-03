4b955e007f6f5ac12493fe25636a7921188e8d83
Leandro Sousa <lgleandro14@gmail.com>
2023-03-03 13:16:39 -0300

    Refatorando exercicio de funções

--- fundamentos/secao-03-introducao-a-javascript/dia-3-javascript-funcoes/script2.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-3-javascript-funcoes/script2.js
@@ -1,16 +1,4 @@
 let clientesTrybeBank = ['Ada', 'John', 'Gus'];
-/* 
-function cadastrarCliente(nome) {
-  if (typeof nome === 'string') {
-    clientesTrybeBank.push(nome);
-    return 'Cliente adicionado';
-  } else {
-    return 'Não é um cliente';
-  }
-}
-console.log(cadastrarCliente('Rui'));
-console.log(clientesTrybeBank); */
-
 
 let tipoVerificado = false;
 
