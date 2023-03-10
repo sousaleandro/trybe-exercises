6893ad8627d0f4213f3415fc4eb271c9e65b62ff
Leandro Sousa <lgleandro14@gmail.com>
2023-03-10 17:22:27 -0300

    Atualiza exercicio final

--- fundamentos/secao-04-javascript-dom-eventos-web-storage/dia-2-javascript-elementos-itens-subitens/script.js
+++ fundamentos/secao-04-javascript-dom-eventos-web-storage/dia-2-javascript-elementos-itens-subitens/script.js
@@ -1,11 +1,12 @@
 const getBody = document.getElementsByTagName('body');
 
-// function tagCreator() {
-//   const getTag = document.createElement('h1');
-//   getTag.innerHTML = 'TrybeTrip - Agência de Viagens';
-//   getBody.appendChild(getTag);
-// };
 
-// tagCreator();
-// console.log(document.getElementsByTagName('h1').innerHTML);
-console.log(document.getElementsByTagName('head'));
+function tagCreator() {
+  const createTag = document.createElement('h1');
+  createTag.innerHTML = 'TrybeTrip - Agência de Viagens';
+  getBody.appendChild(createTag);
+};
+
+
+
+
