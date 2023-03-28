7b676d83234b02807beb12de932cf04648e0c75c
Leandro Sousa <lgleandro14@gmail.com>
2023-03-28 16:01:04 -0300

    Finaliza exericicio parte 2

--- fundamentos/secao-06-javascript-es6-e-testes-unitarios/dia-1-fluxo-de-excecoes-e-manipulacao-de-objetos/script.js
+++ fundamentos/secao-06-javascript-es6-e-testes-unitarios/dia-1-fluxo-de-excecoes-e-manipulacao-de-objetos/script.js
@@ -1,75 +1,166 @@
-// Primeira parte
-const promo = [
-  {product: 'bola de beach tennis', price: 29.99},
-  {product: 'mochila de trilha 20 litros', price: 120.99},
-  {product: 'capacete de ciclista', price: 180.59},
-  {product: 'toalha compacta de secagem', price: 39.99},
-  {product: 'kit 5 camisetas Dry Fit', price: 99.99},
-  {product: 'caneleiras de peso - 3kg (par)', price: 44.99},
-  {product: 'bola de ginástica e pilates', price: 149.59},
-  {product: 'óculos de natação', price: 18.99},
-  {product: 'capa protetora de prancha de surf', price: 89.59},
-  {product: 'bola de basquete', price: 39.99},
+const clients = [
+  {
+    name: 'João da Silva',
+    age: 28,
+    email: 'joao.silva@gmail.com',
+    phone: '(11) 5555-5555',
+    address: {
+      street: 'Rua dos Lírios',
+      number: 123,
+      neighborhood: 'Jardim Primavera',
+      city: 'São Paulo',
+      state: 'SP',
+      cep: '01234-567',
+    },
+  },
+  {
+    name: 'Maria Souza',
+    age: 35,
+    email: 'maria.souza@hotmail.com',
+    address: {
+      street: 'Rua dos Cravos',
+      number: 456,
+      neighborhood: 'Copacabana',
+      city: 'Rio de Janeiro',
+      state: 'RJ',
+      cep: '12345-678',
+    },
+  },
+  {
+    name: 'Pedro Oliveira',
+    age: 42,
+    email: 'pedro.oliveira@gmail.com',
+    address: {
+      street: 'Rua das Margaridas',
+      number: 789,
+      neighborhood: 'Boa Viagem',
+      city: 'Recife',
+      state: 'PE',
+      cep: '23456-789',
+    },
+  },
+  {
+    name: 'Ana Santos',
+    age: 25,
+    email: 'ana.santos@gmail.com',
+    phone: '(71) 5555-5555',
+    address: {
+      street: 'Rua dos Girassóis',
+      number: 1011,
+      neighborhood: 'Barra',
+      city: 'Salvador',
+      state: 'BA',
+      cep: '34567-890',
+    },
+  },
+  {
+    name: 'Luiz Costa',
+    age: 32,
+    email: 'luiz.costa@hotmail.com',
+    address: {
+      street: 'Rua das Acácias',
+      number: 1213,
+      neighborhood: 'Centro',
+      city: 'Belo Horizonte',
+      state: 'MG',
+      cep: '45678-901',
+    },
+  },
+  {
+    name: 'Isabela Almeida',
+    age: 37,
+    email: 'isabela.almeida@gmail.com',
+    phone: '(21) 5555-5555',
+    address: {
+      street: 'Rua das Hortênsias',
+      number: 1415,
+      neighborhood: 'Botafogo',
+      city: 'Rio de Janeiro',
+      state: 'RJ',
+      cep: '56789-012',
+    },
+  },
+  {
+    name: 'Rafael Ferreira',
+    age: 29,
+    email: 'rafael.ferreira@hotmail.com',
+    address: {
+      street: 'Rua das Orquídeas',
+      number: 1617,
+      neighborhood: 'Pinheiros',
+      city: 'São Paulo',
+      state: 'SP',
+      cep: '67890-123',
+    },
+  },
 ];
 
-const button = document.querySelector('#send-button');
-
-button.addEventListener('click', () => {
-  const name = document.querySelector('#name-id').value;
-  const number = document.querySelector('#number-id').value;
-
-  showPromo(name, number);
-});
-
-const showPromo = (name, number) => {
-  const firstText = document.querySelector('#text-initial');
-  const secondText = document.querySelector('#text-final');
-  
-  try {
-    checkName(name);
-    checkNumber(parseInt(number));
-    checkValidRange(number);
-    const productObject = checkPromo(number);
-
-    firstText.innerHTML = `Boas-vindas, ${name}!`;
-    secondText.innerHTML = `A promoção do dia é: 
-      ${productObject.product} no valor de R$ ${productObject.price}`;
-  } catch(err) {
-    secondText.innerHTML = err.message;
-  } finally {
-  document.querySelector('#name-id').value = "";
-  document.querySelector('#number-id').value = "";
-  }
-}
-
-// Segunda parte
-const checkNumber = (number) => {
-  if (isNaN(number)) {
-    throw new Error('É necessário digitar um número');
-  }
-}
-
-// Terceira parte
-const checkPromo = (number) => {
-  for (let index = 0; index < promo.length; index += 1) {
-    if (number - 1 === index) {
-      return promo[index];
+const checkPersonName = (name) => {
+  let getPerson;
+  clients.forEach((clients) => {
+    if (name === clients.name) {
+      getPerson = clients.name;
     }
+  })
+  if (!getPerson) {
+    throw new Error(('Pessoa não encontrada, tente novamente'));
   }
-}
+};
 
-// Exercicio
-const checkValidRange = (number) => {
-  if (number < 1 || number > 10) {
-    throw new Error('O número deve ser de 1 á 10!')
-  }
-}
 
-// Quarta parte
-const checkName = (name) => {
-  let letters = /[aA-zZ]+/;
-  
-  if (!name.match(letters)) {
-    throw new Error('É necessário digitar um nome válido');
+const findPersonByName = (name) => {
+  try {
+    checkPersonName(name);
+    clients.forEach((clients) => {
+      if (name === clients.name)
+        return console.log(`Destinatário: ${clients.name}. Endereço: ${clients.address.street}, ${clients.address.number}, ${clients.address.neighborhood}, ${clients.address.city} - ${clients.address.state}. CEP: ${clients.address.cep}`);
+    })
+  } catch (erro) {
+    return console.log(erro.message);
   }
-}
+};
+
+const checkPersonPosition = (position) => {
+  if (position < 0 || position >= clients.length) {
+    throw new Error('Posição inválida, tente novamente');
+  }
+};
+
+const findPersonByPosition = (position) => {
+  try {
+    checkPersonPosition(position);
+    return console.log(`Cliente: ${clients[position].name}. email: ${clients[position].email}`)
+  } catch (erro) {
+    return console.log(erro.message);
+  }
+};
+
+const checkPersonState = (state) => {
+  const people = [];
+  clients.forEach((clients) => {
+    if (clients.address.state === state) {
+      people.push(clients.name)
+    }
+  })
+  if (people.length < 1) {
+    throw new Error('Ops, nenhuma pessoa mora nesse estado, tente outro');
+  }
+};
+
+const findPeopleByState = (state) => {
+  try {
+    checkPersonState(state);
+    const people = [];
+    clients.forEach((clients) => {
+      if (clients.address.state === state) {
+        people.push(clients.name)
+      }
+    })
+    return console.log(people);
+  } catch (erro) {
+    return console.log(erro.message);
+  }
+};
+
+findPeopleByState('SP');
+findPeopleByState('AM');
