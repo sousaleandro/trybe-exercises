42b4b7628d1db07e05f62807b07d44a23d4e6644
Leandro Sousa <lgleandro14@gmail.com>
2023-04-09 13:33:42 -0300

    Finaliza exercicio 3, 4 e 5

--- fundamentos/secao-07-hofs-javascript-es6/dia-4-spread-rest-object-destructuring/script.js
+++ fundamentos/secao-07-hofs-javascript-es6/dia-4-spread-rest-object-destructuring/script.js
@@ -18,3 +18,109 @@ const sum = (...arr) => arr.reduce((acc, crr) => acc + crr, 0);
 console.log(sum(4, 5, 6));
 
 // Exercicio 3
+
+// name: nome da pessoa, age: idade, likes: gosta de, nationality: nacionalidade
+
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+
+// complete a assinatura da função abaixo
+const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+// <nome> tem <anos de idade> e gosta de <gostos da pessoa>
+
+// Retornos esperados:
+console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+
+// Exercicio 4
+
+const peoples = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+const filterPeople = (arr) => {
+  const filteredPeople = arr.filter(({ bornIn, nationality}) => nationality === 'Australian' && bornIn < 2001).map(({ name }) => name);
+  return filteredPeople;
+};
+console.log(filterPeople(peoples));
+
+// Exercicio 5
+
+const ships = [
+  {
+    name: 'Titanic',
+    length: 269.1,
+    measurementUnit: 'meters',
+  },
+  {
+    name: 'Queen Mary 2',
+    length: 1132,
+    measurementUnit: 'feet',
+  },
+  {
+    name: 'Yamato',
+    length: 256,
+    measurementUnit: 'meters',
+  },
+  // measurementUnit: unidade de medida
+];
+
+const shipLength = ({ name, length, measurementUnit }) => {
+  return `${name} is ${length} ${measurementUnit} long`
+};
+
+// retorno esperado
+console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
+console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
+console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
+
+// Exercicio 6
+
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
+
+const { spring, summer, autumn, winter } = yearSeasons
+
+const months = [...spring, ...summer, ...autumn, ...winter];
+
+console.log(months);
