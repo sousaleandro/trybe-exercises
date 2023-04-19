036433e5e1f950bdd141e0261dfa4e53bc26153d
Leandro Sousa <lgleandro14@gmail.com>
2023-04-19 16:23:07 -0300

    Correções da ferramenta

--- front-end/secao-01-introducao-front-end-javascript-assincrono/dia-1-ambiente-de-desenvolvimento/cpf-validation/src/main.js
+++ front-end/secao-01-introducao-front-end-javascript-assincrono/dia-1-ambiente-de-desenvolvimento/cpf-validation/src/main.js
@@ -1,21 +1,20 @@
 import validator from 'validator';
 
-const textInput = document.querySelector('#camp-input');
-const selection = document.querySelector('#type-input');
-const validateBtn = document.querySelector('#validate-button');
-const resultOutput = document.querySelector('#result-output');
+const textInput = document.querySelector('#camp');
+const selector = document.querySelector('#type');
+const validateBtn = document.querySelector('#button');
+const resultOutput = document.querySelector('#output');
 
 validateBtn.addEventListener('click', (event) => {
   event.preventDefault();
-  console.log('gg');
 
-  // const camps = {
-  //   cpf: validator.isTaxID(textInput.value, ['pt-BR']),
-  //   email: validator.isEmail(textInput.value),
-  //   birthDate: validator.isDate(textInput.value, { format: 'DD/MM/YYY', delimiters: ["/"], strictMode: true  }),
-  //   phoneNumber: validator.isMobilePhone(textInput.value ['pt-BR']),
-  //   url: validator.isURL(textInput.value),
-  // };
+  const camps = {
+    cpf: validator.isTaxID(textInput.value, ['pt-BR']),
+    email: validator.isEmail(textInput.value),
+    birthDate: validator.isDate(textInput.value),
+    phoneNumber: validator.isMobilePhone(textInput.value, 'pt-BR'),
+    url: validator.isURL(textInput.value),
+  };
   
-  // resultOutput.innerHTML = `Your input validation is ${camps[selection.value]}`;
-})
+  resultOutput.innerHTML = `Your input validation is ${camps[selector.value]}`
+});
