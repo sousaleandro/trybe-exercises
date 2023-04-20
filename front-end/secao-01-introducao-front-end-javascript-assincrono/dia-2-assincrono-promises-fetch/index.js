67736ce063af65f079e638a2d0d6c9f5688e44aa
Leandro Sousa <lgleandro14@gmail.com>
2023-04-20 16:12:54 -0300

    Finaliza exercicios dia 2 javascript assincrono

---
+++ front-end/secao-01-introducao-front-end-javascript-assincrono/dia-2-assincrono-promises-fetch/index.js
@@ -0,0 +1,43 @@
+import './style.css';
+
+import {
+  fillUsersSelect,
+  fillPosts,
+  fillFeaturedPostComments,
+  clearPageData,
+  fillErrorMessage,
+} from './utils/updateUI';
+
+const usersSelect = document.querySelector('#users-select');
+
+const USERS_API = 'https://dummyjson.com/users';
+// faça a lógica para pegar as informações das pessoas usuárias e preencher o select aqui.
+
+usersSelect.addEventListener('change', () => {
+  clearPageData();
+
+  // faça a lógica para pegar as informações dos posts da pessoa selecionada e dos comentários do post destacado aqui.
+});
+
+usersSelect.addEventListener('change', () => {
+  clearPageData();
+
+  const POSTS_API = `https://dummyjson.com/posts/user/${usersSelect.value}`;
+
+  fetch(POSTS_API)
+    .then((response) => response.json())
+    .then((data) => {
+      const { posts } = data;
+      fillPosts(posts);
+
+      const [featuredPost] = posts;
+      const COMMENTS_API = `https://dummyjson.com/posts/${featuredPost.id}/comments`;
+
+      return fetch(COMMENTS_API);
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      const { comments } = data;
+
+    });
+});
