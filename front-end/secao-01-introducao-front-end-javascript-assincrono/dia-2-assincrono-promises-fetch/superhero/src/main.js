55152d93da9a90fbf5dd258dc5dfe00705f04855
Leandro Sousa <lgleandro14@gmail.com>
2023-04-24 13:22:39 -0300

    Finaliza exercicio api de superhero do course

--- front-end/secao-01-introducao-front-end-javascript-assincrono/dia-2-assincrono-promises-fetch/superhero/src/main.js
+++ front-end/secao-01-introducao-front-end-javascript-assincrono/dia-2-assincrono-promises-fetch/superhero/src/main.js
@@ -1,5 +1,38 @@
+import Swal from 'sweetalert2';
+
+const drafterBtn = document.querySelector('.drafter-btn');
+const heroName = document.querySelector('.hero-name');
+const heroImg = document.querySelector('.hero-img');
+const API_LENGTH = 732;
+
 const fetchHero = (id) => fetch(`https://akabab.github.io/superhero-api/api/id/${id}.json`)
   .then((request) => request.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    if (id !== data.id) {
+      throw new Error('O id não representa um héroi');
+    }
+    return data;
+  });
 
-fetchHero('1');
+drafterBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  heroName.innerHTML = '';
+  heroImg.src = '';
+  const id = Math.round(Math.random() * (API_LENGTH - 1) + 1);
+  fetchHero(id)
+    .then((hero) => {
+      const { name, images } = hero;
+
+      heroName.innerHTML = name;
+      heroImg.src = images.lg;
+    })
+    .catch((error) => {
+      heroName.innerHTML = '';
+      heroImg.src = '';
+      return Swal.fire({
+        title: 'Erro',
+        text: error.message,
+        icon: 'error',
+      });
+    });
+});
