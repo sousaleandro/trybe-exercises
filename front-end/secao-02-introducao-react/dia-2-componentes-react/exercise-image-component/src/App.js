365f1f91745b46736a2d328a78f91b5400fcff49
Leandro Sousa <lgleandro14@gmail.com>
2023-05-03 16:31:39 -0300

    Exercicios do course dia 2 componentes react

---
+++ front-end/secao-02-introducao-react/dia-2-componentes-react/exercise-image-component/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Image from './Image';
+import Cat from './Cat.jpg'
+
+
+function App() {
+  return (
+    <>
+    <Image source={ Cat } alternativeText="Cute cat staring"/>
+    </>
+  );
+}
+
+export default App;
