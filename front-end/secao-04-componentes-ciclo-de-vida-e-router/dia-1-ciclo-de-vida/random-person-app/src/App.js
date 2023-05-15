585ea18a9e5ebf2b948c2223fae56747223c7113
Leandro Sousa <lgleandro14@gmail.com>
2023-05-15 13:05:50 -0300

    Finaliza exercicio de fixação

--- front-end/secao-04-componentes-ciclo-de-vida-e-router/dia-1-ciclo-de-vida/random-person-app/src/App.js
+++ front-end/secao-04-componentes-ciclo-de-vida-e-router/dia-1-ciclo-de-vida/random-person-app/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = 'https://api.randomuser.me';
+const limitAge = 50;
+
+class App extends Component {
+  state = {
+    random: undefined,
+    loading: true,
+    allRandoms: [],
+  };
+
+  async componentDidMount() {
+    console.log('montou');
+    await this.fetchRandom();
+  }
+
+  shouldComponentUpdate(_nextProps, nextState) {
+    if (nextState.random[0].dob.age < limitAge) return true;
+  }
+
+  fetchRandom = async () => {
+    const request = await fetch(API_URL);
+    const data = await request.json();
+    this.setState(() => ({
+      loading: false,
+      random: data.results,
+    }))
+  }
+
+  render() {
+    const { loading, random } = this.state;
+    // const { name: { title, first, last }, email, dob: { age }, picture: { medium } } = random[0];
+    console.log('renderizou');
+    if (loading) return <p>Loading...</p>;
+    return (
+      <div>
+        <img src={random[0].picture.medium} alt={random[0].name.first} />
+        <p>{`${random[0].name.title} ${random[0].name.first} ${random[0].name.last}`}</p>
+        <p>{random[0].email}</p>
+        <p>{random[0].dob.age}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
