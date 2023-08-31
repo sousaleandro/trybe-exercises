c1f72c030f324780e6066a3b4ce872b415d92efd
Leandro Sousa <lgleandro14@gmail.com>
2023-08-30 22:08:58 -0300

    finaliza exercicios dia 4 node

--- back-end/secao-04-node-js/dia-2-api-rest-express/soccer-team-manager/src/app.js
+++ back-end/secao-04-node-js/dia-2-api-rest-express/soccer-team-manager/src/app.js
@@ -1,65 +1,53 @@
-// src/app.js
-
 const express = require('express');
+require('express-async-errors');
+const validateTeam = require('./middlewares/validateTeam');
+const validateId = require('./middlewares/validateId');
+const teams = require('./utils/teams');
+const apiCredentials = require('./middlewares/apiCredentials');
 
 const app = express();
 
 let nextId = 3;
-const teams = [
-  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
-  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
-];
-
 app.use(express.json());
-
-const validateTeam = (req, res, next) => {
-  const requiredProperties = ['nome', 'sigla'];
-  if (requiredProperties.every((property) => property in req.body)) {
-    next();
-  } else {
-    res.sendStatus(400);
-  }
-};
+app.use(apiCredentials);;
 
 app.get('/teams', (req, res) => res.json(teams));
 
-app.get('/teams/:id', (req, res) => {
+app.get('/teams/:id', validateId, (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find(t => t.id === id);
-  if (team) {
-    res.json(team);
-  } else {
-    res.sendStatus(404);
-  }
+  res.json(team);
 });
 
 app.post('/teams', validateTeam, (req, res) => {
+  if (
+    // confere se a sigla proposta está inclusa nos times autorizados
+    !req.teams.teams.includes(req.body.sigla)
+    // confere se já não existe um time com essa sigla
+    && teams.every((t) => t.sigla !== req.body.sigla)
+  ) {
+    return res.status(422).json({ message: 'Já existe um time com essa sigla'});
+  }
   const team = { id: nextId, ...req.body };
   teams.push(team);
   nextId += 1;
   res.status(201).json(team);
 });
 
-app.put('/teams/:id', validateTeam, (req, res) => {
+app.put('/teams/:id', validateId, validateTeam, (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find(t => t.id === id);
-  if (team) {
-    const index = teams.indexOf(team);
-    const updated = { id, ...req.body };
-    teams.splice(index, 1, updated);
-    res.status(201).json(updated);
-  } else {
-    res.sendStatus(400);
-  }
+  const index = teams.indexOf(team);
+  const updated = { id, ...req.body };
+  teams.splice(index, 1, updated);
+  res.status(201).json(updated);
 });
 
-app.delete('/teams/:id', (req, res) => {
+app.delete('/teams/:id', validateId, (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find(t => t.id === id);
-  if (team) {
-    const index = teams.indexOf(team);
-    teams.splice(index, 1);
-  }
+  const index = teams.indexOf(team);
+  teams.splice(index, 1);
   res.sendStatus(204);
 });
 
