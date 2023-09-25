8430b8340e13c430d536cfa5b439e5449471e41c
Leandro Sousa <lgleandro14@gmail.com>
2023-09-25 18:10:03 -0300

    finaliza exercicios do course dia 3 orm

--- back-end/secao-06-node-orm-auth/dia-2-associations/business/src/services/employee.service.js
+++ back-end/secao-06-node-orm-auth/dia-2-associations/business/src/services/employee.service.js
@@ -1,4 +1,9 @@
 const { Address, Employee } = require('../models/');
+const Sequelize = require('sequelize');
+const config = require('../config/config');
+
+const env = process.env.NODE_ENV || 'development';
+const sequelize = new Sequelize(config[env]);
 
 const getAll = async () => {
   const users = await Employee.findAll({
@@ -13,9 +18,25 @@ const getById = async (id) => {
     where: { id },
   });
   return employee;
-}
+};
+
+const insert = async ({ firstName, lastName, age, city, street, number }) => {
+  const result = await sequelize.transaction(async (t) => {
+    const employee = await Employee.create({
+      firstName, lastName, age,
+    }, { transaction: t });
+
+    await Address.create({
+      city, street, number, employeeId: employee.id
+    }, { transaction: t });
+    return employee;
+  });
+
+  return result;
+};
 
 module.exports = {
   getAll,
   getById,
+  insert,
 };
